779c6b9106df33eb86939391affdcfd36d9260bf
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-04-28 17:45:13 -0300

    server atualizado

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const server = express();
-const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000; // Define a porta do servidor
 
-server.get('/', (req, res) => {
-  res.send('Servidor está funcionando!');
+// Middleware para lidar com solicitações JSON
+app.use(express.json());
+
+// Rota para lidar com o envio do formulário
+app.post('/api/formulario', (req, res) => {
+    const { nome, email, assunto, mensagem } = req.body;
+    // Aqui você pode processar os dados do formulário como desejar
+    console.log('Dados do formulário recebidos:', { nome, email, assunto, mensagem });
+    res.json({ mensagem: 'Dados recebidos com sucesso!' });
 });
 
-server.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
